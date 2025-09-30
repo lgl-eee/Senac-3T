11d6f6507ffa0eb8d2ecb2d21d1462641f71206c
pedro jewur <84276531+lgl-eee@users.noreply.github.com>
2025-09-30 15:30:31 -0300

    Add files via upload

---
+++ Atividade 05/script.js
@@ -0,0 +1,18 @@
+//arrays -> vetores, matrizes 
+//estruturas de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maçã", "Melancia"]; //array tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(10,2,6,7)
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Gaby";
+console.log(arrayVazio);
+
+//tamanho de array -> lenght
+let dados = [32,4,80,6]; //tamanho 5 -> leght 5
+console.log(dados.lenght);
