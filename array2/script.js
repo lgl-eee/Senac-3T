8e8f45a24f7d09fd20964635156f321eac164365
pedro jewur <84276531+lgl-eee@users.noreply.github.com>
2025-10-14 16:10:18 -0300

    Add files via upload

---
+++ array2/script.js
@@ -0,0 +1,7 @@
+let numeros = [1,2,3];
+numeros.push(4,5); //adiciona item no final do array
+console.log(numeros);
+
+let numerais = [1,2]
+numeros.unshift(0); // adiciona no inicio do array
+console.log(numerais);
